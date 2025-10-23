fe8763a938d7bc3e25b936e7604db79d62e5c49b
AHMZ <ahmz.ut@gmail.com>
2025-10-23 13:13:47 +0330

    fix: update health check command in dev Dockerfile and configure  vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig(({ mode }) => {
       react(),
       mode === 'analyze' && visualizer({ filename: 'dist/stats.html', open: true }),
     ].filter(Boolean),
+    server: {
+      host: true,
+      allowedHosts: ['dev.bugsbuzzy', 'localhost', 'bugsbuzzy.ir', 'www.bugsbuzzy.ir'],
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'),
